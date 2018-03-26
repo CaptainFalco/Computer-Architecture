c9b078aafbb8187d7ed055a0bd7e7e7d2694cb4a
Nick Dumont <nicolasdumont1310@gmail.com>
2018-03-26 02:41:00 -0400

    Memory in place
    Extra commands can be added, but this is what we are looking at for the base User Interface.  I'm thinking a step command button too, which would more or less be a derivative of the basic running process so each step can be seen.

--- Memory_Test/simulator.py
+++ Memory_Test/simulator.py
@@ -3,26 +3,31 @@ import tkinter as tk
 
 class Application(tk.Frame):
 
+    def start_simulator(self):
+        print("This will call the start method reference.")
+
+    def pause_simulator(self):
+        print("Set some variable in a running method to a pause condition until otherwise.")
 
     def createWidgets(self):
         # Start the Simulator
         self.Start = tk.Button(self)
         self.Start["text"] = "Start"
-        # self.Start["command"] = self.start_simulator
+        self.Start["command"] = self.start_simulator
         self.Start.grid(row=0, column=0)
 
         # Pause the Simulator
         # Suggestion: Creating a constant check every cycle to see if this button is pressed.
         self.Pause = tk.Button(self)
         self.Pause["text"] = "Pause"
-        # self.Pause["command"] = SET SOME VARIABLE FROM THE OTHER FILE TO BREAK A CONTINUE CONDITION
+        self.Pause["command"] = self.pause_simulator   # SET SOME VARIABLE FROM THE OTHER FILE TO BREAK A CONTINUE CONDITION
         self.Pause.grid(row=0, column=1)
 
 
-        # Reset the Simulator (Basically calling start again
+        # Reset the Simulator (Basically calling start again)
         self.Reset = tk.Button(self)
         self.Reset["text"] = "Reset"
-        self.Reset["command"] = self.updateMemory()
+        self.Reset["command"] = self.start_simulator
         self.Reset.grid(row=0, column=2)
 
         # Closes window
@@ -62,22 +67,53 @@ class Application(tk.Frame):
             self.addressVal["text"] = 0
             self.addressVal.grid(row=6, column=t)
 
-        self.memoryLabel = tk.Label(self)
-        self.memoryLabel["text"] = "Clock Cycles:"
-        self.memoryLabel.grid(row=7, column=0)
+        self.ccLabel = tk.Label(self)
+        self.ccLabel["text"] = "   "
+        self.ccLabel.grid(row=7, column=0)
 
-    def updateMemory(self):
-        index = 0
-        '''for r in range(2,10):
-            for c in range(8):
-                self.memoryLabel = tk.Label(self)
-                self.memoryLabel.grid(row = r, column = c)
-                self.memoryLabel["text"] = memorySpots[index]
-                index = index+1'''
+        self.ccLabel = tk.Label(self)
+        self.ccLabel["text"] = "Cycle Counter:"
+        self.ccLabel.grid(row=8, column=0)
+
+        self.ccValue = tk.Label(self)
+        self.ccValue["text"] = clockCycles
+        self.ccValue.grid(row=8, column=2)
+
+        self.pcCounter = tk.Label(self)
+        self.pcCounter["text"] = "PC Counter:"
+        self.pcCounter.grid(row=9, column=0)
+
+        self.pcValue = tk.Label(self)
+        self.pcValue["text"] = pcCounter
+        self.pcValue.grid(row=9, column=2)
+
+
 
     def edit_memory(self, slot, value):
         memoryValues[slot] = value
         self.updateMemory
+
+    def updateMemory(self):
+        for t in range(1, 33):
+            self.address = tk.Label(self)
+            self.address["text"] = memoryValues[t-1]
+            self.address.grid(row=3, column = t)
+
+    def edit_register(self, slot, value):
+        registerValues[slot] = value
+        self.updateRegisters
+
+    def updateRegisters(self):
+        for t in range(1,9):
+            self.addressVal = tk.Label(self)
+            self.addressVal["text"] = registerValues[t-1]
+            self.addressVal.grid(row=6, column=t)
+
+    def increaseClockCyles(self):
+        clockCyles = self.clockCycles + 1
+
+    def increasePCCounter(self):
+        pcCounter = self.pcCounter + 1
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
@@ -88,6 +124,8 @@ memorySpots = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,
 memoryValues = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 registerSpots = [0,1,2,3,4,5,6,7]
 registerValues = [0,0,0,0,0,0,0,0]
+clockCycles = 0
+pcCounter = 0
 # memorySpots = [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[1,0],[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[2,0],[2,1],[2, 2],[2,3],[2,4],[2,5],[2,6],[2,7],[3,0],[3,1],[3,2],[3,3],[3,4],[3,5],[3,6],[3,7],[4,0],[4,1],[4,2],[4,3],[4,4],[4,5],[4,6],[4,7],[5,0],[5,1],[5,2],[5,3],[5,4],[5,5],[5,6],[5,7],[6,0],[6,1],[6,2],[6,3],[6,4],[6,5],[6,6],[6,7],[7,0],[7,1],[7,2],[7,3],[7,4],[7,5],[7,6],[7,7]]
 
 root = tk.Tk()
